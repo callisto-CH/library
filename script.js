a3f6d2ffe454cea48dfccebc493cde2cc8d6de19
callisto-CH <160159416+callisto-CH@users.noreply.github.com>
2024-08-07 19:50:41 -0400

    Create library app with class-based logic

--- script.js
+++ script.js
@@ -6,129 +6,138 @@ if (window.history.replaceState) {
 };
 // Copied code ends here
 
-let library = [];
+class Book {
 
-function Book(title, author, pages, read) {
-	this.title = title;
-	this.author = author;
-	this.pages = pages;
-	this.read = read;
-};
-
-Book.prototype.readToggle = function() {
-	if (this.read == "Read") {
-		this.read = "Not read";
-	}
-	else {
-		this.read = "Read";
-	}
-}
-
-function addBookToLibrary(title, author, pages, read) {
-	const newBook = new Book(title, author, pages, read);
-	library.push(newBook);
-	updateLibraryDisplay(library.length-1);
-};
-
-function updateLibraryDisplay(startIndex) {
-	const bookGrid = document.querySelector(".book-grid");
-	for (let i = startIndex; i < library.length; i++) {
-		const book = library[i];
-
-		const bookFrag = document.createDocumentFragment();
-		
-		const bookDiv = document.createElement(`div`);
-		bookDiv.classList.add(`book`);
-		bookDiv.setAttribute("id", `${library.length}`);
-
-		const infoArray = [["Title: ", "title"], 
-				   ["Author: ", "author"], 
-				   ["Pages: ", "pages"], 
-				   [``, "read"]];
-		infoArray.forEach((info) => {
-			const bookInfo = document.createElement("div");
-			if (info[0] == "Title: ") {
-				const span = document.createElement("span");
-				bookInfo.textContent = info[0];
-				span.textContent = `${book[info[1]]}`;
-				bookInfo.appendChild(span);
-			}
-			else {
-				bookInfo.textContent = info[0] + `${book[info[1]]}`;
-			}
-			bookInfo.classList.add(info[1]);
-			bookDiv.appendChild(bookInfo);
-		});
-		
-		const deleteButton = document.createElement(`button`);
-		deleteButton.textContent = "Delete";
-		deleteButton.classList.add("deleteButton");
-		deleteButton.addEventListener("click", () => {
-			const deleteId = deleteButton.parentNode.getAttribute("id");
-			library.splice(deleteId-1,1);
-			deleteButton.parentNode.remove();
-			document.querySelectorAll(".book").forEach((entry) => {
-				const entryId = +entry.getAttribute("id");
-				if (entryId > deleteId) {
-					entry.setAttribute("id", entryId-1);
-				};
-			});
-		});
-		
-		const readButton = document.createElement(`button`);
-		readButton.textContent = "Toggle read";
-		readButton.classList.add(`readButton`);
-		readButton.addEventListener("click", () => {
-			const entryId = +readButton.parentNode.getAttribute("id");
-			library[entryId-1].readToggle();
-			let text = readButton.parentNode.querySelector(".read").textContent;
-			if (text == "Read") {
-				text = "Not Read"
-			}
-			else {
-				text = "Read"
-			};
-			readButton.parentNode.querySelector(".read").textContent = text;
-			console.log(library);
-		});
-		
-		bookDiv.appendChild(readButton);
-		bookDiv.appendChild(deleteButton);
-		
-		bookFrag.appendChild(bookDiv);
-		bookGrid.appendChild(bookFrag);
+	constructor(title, author, pages, read) {
+		this.title = title;
+		this.author = author;
+		this.pages = pages;
+		this.read = read;
 	};
+
+	readToggle() {
+		this.read = this.read == "Read" ? "Not read" : "Read";
+	};
+
 };
 
-const form = document.querySelector("dialog");
+class Library {
 
-const addBook = document.querySelector(".add-book");
-addBook.addEventListener("click", () => {
-	form.showModal();
-});
+	constructor() {
+		this.contents = [];
+	};
 
-const closeForm = document.querySelector(".close-form");
-closeForm.addEventListener("click", () => {
-	form.close();
-});
+	addBook(book) {
+		this.contents.push(book);
+		this.render(this.contents.length-1);
+	};
 
-form.addEventListener("submit", submitHandler);
-function submitHandler (event) {
-	event.preventDefault();
-	addBookToLibrary(document.getElementById("title").value,
-			document.getElementById("author").value,
-			document.getElementById("pages").value,
-			document.getElementById("read").checked ? "Read" : "Not read");
-	document.getElementById("title").value = "";
-	document.getElementById("author").value = "";
-	document.getElementById("pages").value = "";
-	document.getElementById("read").checked = false;
-	updateLibraryDisplay(library.length);
-	form.close();
+	render(startIndex) {
+		const bookGrid = document.querySelector(".book-grid");
+		for (let i = startIndex; i < this.contents.length; i++) {
+			const book = this.contents[i];
+	
+			const bookFrag = document.createDocumentFragment();
+		
+			const bookDiv = document.createElement(`div`);
+			bookDiv.classList.add(`book`);
+			bookDiv.setAttribute("id", `${this.contents.length}`);
+
+			const infoArray = [["Title: ", "title"], 
+					   ["Author: ", "author"], 
+					   ["Pages: ", "pages"], 
+					   [``, "read"]];
+			infoArray.forEach((info) => {
+				const bookInfo = document.createElement("div");
+				if (info[0] == "Title: ") {
+					const span = document.createElement("span");
+					bookInfo.textContent = info[0];
+					span.textContent = `${book[info[1]]}`;
+					bookInfo.appendChild(span);
+				}
+				else {
+					bookInfo.textContent = info[0] + `${book[info[1]]}`;
+				}
+				bookInfo.classList.add(info[1]);
+				bookDiv.appendChild(bookInfo);
+			});
+		
+			const deleteButton = document.createElement(`button`);
+			deleteButton.textContent = "Delete";
+			deleteButton.classList.add("deleteButton");
+			deleteButton.addEventListener("click", () => {
+				const deleteId = deleteButton.parentNode.getAttribute("id");
+				this.contents.splice(deleteId-1,1);
+				deleteButton.parentNode.remove();
+				document.querySelectorAll(".book").forEach((entry) => {
+					const entryId = +entry.getAttribute("id");
+					if (entryId > deleteId) {
+						entry.setAttribute("id", entryId-1);
+					};
+				});
+			});
+		
+			const readButton = document.createElement(`button`);
+			readButton.textContent = "Toggle read";
+			readButton.classList.add(`readButton`);
+			readButton.addEventListener("click", () => {
+				const entryId = +readButton.parentNode.getAttribute("id");
+				this.contents[entryId-1].readToggle();
+				let text = readButton.parentNode.querySelector(".read").textContent;
+				text = text == "Read" ? "Not Read" : "Read";
+				readButton.parentNode.querySelector(".read").textContent = text;
+			});
+		
+			bookDiv.appendChild(readButton);
+			bookDiv.appendChild(deleteButton);
+		
+			bookFrag.appendChild(bookDiv);
+			bookGrid.appendChild(bookFrag);
+		};
+	};
+
 };
 
-addBookToLibrary("Of Mice and Men", "John Steinbeck", 107, "Read");
+const library = new Library();
 
-addBookToLibrary("The Stranger", "Albert Camus", 159, "Read");
+class Form {
 
-addBookToLibrary("Mistborn: The Final Empire", "Brandon Sanderson", 647, "Not read");
+	constructor() {
+		this.modal = document.querySelector("dialog");
+		this.addBookBtn = document.querySelector(".add-book");
+		this.closeFormBtn = document.querySelector(".close-form");
+		this.title = document.getElementById("title");
+		this.author = document.getElementById("author");
+		this.pages = document.getElementById("pages");
+		this.read = document.getElementById("read")
+	};
+
+	initialize() {
+		this.addBookBtn.addEventListener("click", () => {
+			this.modal.showModal();
+		});
+		this.closeFormBtn.addEventListener("click", () => {
+			this.modal.close();
+		});
+		this.modal.addEventListener("submit", (event) => {
+			event.preventDefault();
+			library.addBook(new Book(form.title.value, form.author.value, form.pages.value, form.read.checked ? "Read" : "Not read"));
+			form.title.value = "";
+			form.pages.value = "";
+			form.author.value = "";
+			form.pages.value = "";
+			form.read.checked = false;
+			form.modal.close();
+		});
+	};
+
+};
+
+const form = new Form();
+form.initialize();
+
+library.addBook(new Book("Of Mice and Men", "John Steinbeck", 107, "Read"));
+
+library.addBook(new Book("The Stranger", "Albert Camus", 159, "Read"));
+
+library.addBook(new Book("Mistborn: The Final Empire", "Brandon Sanderson", 647, "Not read"));
